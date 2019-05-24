9b6963678110aa691c488b8b8b95d45054ca9ed0
Pablo Infantes <pabloinfantes@gmail.com>
2019-05-24 12:45:25 +0200

    He añadido dos test

--- EjercicioIBAN/IbanTest/IbanTest.cs
+++ EjercicioIBAN/IbanTest/IbanTest.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using IbanTest;
+using SolucionBancaria;
 using NUnit.Framework;
 
 namespace IbanTest
@@ -11,6 +11,33 @@ namespace IbanTest
     [TestFixture]
     public class IbanTest
     {
+        [Test]
+        public void LaCuentaNoTieneLongitudValida()
+        {
+            try
+            {
+                string CuentaBanco = "00492352082414205416";
 
+                IBAN.esUnCCValido(CuentaBanco);
+                Assert.Fail("Numero Incorrecto");
+            }
+            catch (LongitudIncorrectaException)
+            {
+                //Tiene que fallar la longitud de la cuenta
+            }
+        }
+
+        [Test]
+        public void LaCuentaEsValida()
+        {
+            
+                string CuentaBanco = "00492352082414205416";
+
+                IBAN.esUnCCValido(CuentaBanco);
+                Assert.IsTrue(IBAN.esUnCCValido(CuentaBanco));
+                
+
+        
+        }
     }
 }
